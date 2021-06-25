f2d6504ad8dd1af54746f09f510b0a0067f6600f
Needa Mehmud <needa.mehmud@gmail.com>
2021-06-25 10:28:25 -0400

    added fuctionality to save text locally

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,17 @@
-var text = document.getElementById('#text');
-var hours = document.getElementById('#hour')
-var saveButton = document.getElementsByClassName('saveButton');
+var text = $('#text');
+var hours = $('#hour')
+var saveButtons = $('.saveButton');
 
+//current time and date
 var currentDay = moment().format("dddd, MMMM Do YYYY, h:mm:ss a");
 $("#current-time").append(currentDay);
 
+//saving text to local storage
+saveButtons.each(function () {
+    $(this).click(function () {
+        console.log($(this).siblings('textarea').attr('id'))
+        var textarea = $(this).siblings('textarea');
+        localStorage.setItem(textarea.attr('id'), textarea.val())
+    });
+})
 
