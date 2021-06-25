dc5cec1736cce6f5795a201154600de9f35ca246
Needa Mehmud <needa.mehmud@gmail.com>
2021-06-25 11:47:48 -0400

    added logic to color coding the schedule

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,31 @@
-var text = $('#text');
-var hours = $('#hour')
 var saveButtons = $('.saveButton');
 
 //current time and date
 var currentDay = moment().format("dddd, MMMM Do YYYY, h:mm:ss a");
-$("#current-time").append(currentDay);
+var currentTime = moment().format('ha')
+$('#current-time').append(currentDay);
+
+
+//color coding the schedule
+$('textarea').each(function () {
+    var textareaId = $(this).attr('id') //9am
+    var scheduleTime = moment(textareaId, 'ha');
+    console.log(scheduleTime);
+
+    if (moment(scheduleTime).isAfter(moment())) {
+        $(this).addClass('bg-success')
+    } else if (moment(scheduleTime).isBefore(moment())) {
+        $(this).addClass('bg-secondary')
+    } else {
+        $(this).addClass('bg-danger')
+    }
+
+    if (textareaId === currentTime) {
+        $(this).removeClass('bg-success');
+        $(this).removeClass('bg-secondary');
+        $(this).addClass('bg-danger');
+    }
+});
 
 //saving text to local storage
 saveButtons.each(function () {
